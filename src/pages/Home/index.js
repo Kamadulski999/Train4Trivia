7c6b4969c1bf1465d448f3db87729d72b2871ade
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-03 07:25:57 -0600

    conditional rendering of modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,17 @@ import Modal from '../../components/Modal';
 
 
 const Home = () => {
+
+    // gameOn is a placeholder for a state function to be added later. 
+    // gameOn false will render the homepade. gameOn true will render the game modal.
+    const gameOn = true;
     return (
         <>
         <div className="container">
           <div className="banner neonText">
             Trivia Trainer</div>           
         </div>
-        <Modal/>
+        {gameOn && <Modal/>}
         </>
     )
 }
