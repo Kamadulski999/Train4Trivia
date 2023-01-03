1b0f8e193b93cd8d50e9aaeddecda0a4c0d0bef5
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-03 07:19:41 -0600

    modal component skeleton

---
+++ src/components/Modal/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './modal.css'
+
+function Modal() {
+    return(
+        <div className="modal">
+            Modal goes here!!!!!
+        </div>
+    )
+}
+
+export default Modal;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
-import './home.css'
+import './home.css';
+import Modal from '../../components/Modal';
 
 
 const Home = () => {
     return (
+        <>
         <div className="container">
           <div className="banner neonText">
             Trivia Trainer</div>           
         </div>
+        <Modal/>
+        </>
     )
 }
 
