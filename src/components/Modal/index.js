0a23c5e17d495afead9d9b1127b371eae7a2c034
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-03 08:24:01 -0600

    added dropdowns

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,7 +4,22 @@ import './modal.css'
 function Modal() {
     return(
         <div className="game-start">
-            <div className="text-danger color-me"> Hello World</div>
+            <div className="select-container">
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Choose a Category</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            </select>             
+            </div>
+            <div className="select-container">
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Choose the number of Questions</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            </select> 
+            </div>
         </div>
     )
 }
