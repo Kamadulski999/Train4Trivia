296a6de9ea6059416f96b339ea9acd0cda7f0904
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-03 08:42:01 -0600

    finish landing page layout

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,21 +4,41 @@ import './modal.css'
 function Modal() {
     return(
         <div className="game-start">
-            <div className="select-container">
-            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option selected>Choose a Category</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-            </select>             
+            <div className="select-label">
+                Choose your Category
             </div>
             <div className="select-container">
             <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option selected>Choose the number of Questions</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option selected>Categories</option>
+            <option value="">Random</option>
+            <option value="9">General</option>
+            <option value="25">Art & Literature</option>
+            <option value="10">Language</option>
+            <option value="17">Science and Nature</option>
+            <option value="26">People and Places</option>
+            <option value="23">History Holidays</option>
+            <option value="14">Entertainment</option>
+            <option value="16">Toys and Games</option>
+            <option value="12">Music</option>
+            <option value="20">Religion and Mythology</option>
+            <option value="21">Sports and Liesure</option>            
+            </select>             
+            </div>
+            <div className="select-label">
+                Choose the Number of Questions
+            </div>
+            <div className="select-container">
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Questions</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="40">40</option>
+            <option value="50">50</option>
             </select> 
+            </div>
+            <div className="start-button">
+            <button type="button" class="btn btn-primary">Start Training!</button>
             </div>
         </div>
     )
