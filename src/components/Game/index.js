84a42f05b14b13031712c3eee4b7b2f23f703e04
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-04 17:44:24 -0600

    game modal layout

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,19 +3,50 @@ import './game.css';
 
 function Game({gameOn,setGameOn, questions, setQuestions}) {
     const quitGame = () => {
-        setGameOn(false)
-        console.log(questions)
+        setGameOn(false)        
     }
     
+    // replace unicode characters
+    function unEscape(htmlStr) {
+        htmlStr = htmlStr.replace(/&lt;/g , "<");	 
+        htmlStr = htmlStr.replace(/&gt;/g , ">");     
+        htmlStr = htmlStr.replace(/&quot;/g , "\"");  
+        htmlStr = htmlStr.replace(/&#039;/g , "\'");   
+        htmlStr = htmlStr.replace(/&amp;/g , "&");
+        return htmlStr;
+    }  
+    
+    var questStr = ""
+    if (questions) {
+        questStr = unEscape(JSON.stringify(questions[0].question))             
+        }
+        
+    const checkAnswer = () => {
+        console.log("checking answer")
+    }
+        
    
-return (
+        
+    
+
+    
+  
+    return (
    
     <>
     {!questions && <div className="loading neonText">Loading....</div>}
     <div className="quit-game">
             <button type="button" onClick={quitGame} className="btn btn-primary">Quit Game</button>
             </div>
-    {questions && <div className="game-modal">question</div> }
+    {questions && <div className="game-modal">
+        <div className="question-container">{questStr}</div>
+        <div className='answer-container'>
+            <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 1</button></div>
+            <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 2</button></div>
+            <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 3</button></div>
+            <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 4</button></div>
+        </div> 
+    </div> }
     </>
 )
 }
