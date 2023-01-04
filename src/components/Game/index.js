43d71ec3797be36f2c06d94ab46acc52c24ffd3b
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-04 17:58:28 -0600

    added question counter

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import './game.css';
 
 function Game({gameOn,setGameOn, questions, setQuestions}) {
-    const quitGame = () => {
-        setGameOn(false)        
-    }
     
-    // replace unicode characters
+    // counter determines the question that is displayed
+    const [counter, setCounter] = useState(0)
+   
+    // reformat unicode escape characters from API
     function unEscape(htmlStr) {
         htmlStr = htmlStr.replace(/&lt;/g , "<");	 
         htmlStr = htmlStr.replace(/&gt;/g , ">");     
@@ -18,26 +18,24 @@ function Game({gameOn,setGameOn, questions, setQuestions}) {
     
     var questStr = ""
     if (questions) {
-        questStr = unEscape(JSON.stringify(questions[0].question))             
+        questStr = unEscape(JSON.stringify(questions[counter].question))             
         }
         
     const checkAnswer = () => {
-        console.log("checking answer")
+        setCounter(counter + 1)
+        console.log(counter)
     }
-        
-   
-        
-    
 
-    
+    const quitGame = () => {
+        setGameOn(false)        
+    }                  
   
-    return (
-   
+    return (   
     <>
+    {/* show loading screen until fetch is complete */}
     {!questions && <div className="loading neonText">Loading....</div>}
-    <div className="quit-game">
-            <button type="button" onClick={quitGame} className="btn btn-primary">Quit Game</button>
-            </div>
+    
+    {/* after fetch show questions */}
     {questions && <div className="game-modal">
         <div className="question-container">{questStr}</div>
         <div className='answer-container'>
@@ -46,7 +44,11 @@ function Game({gameOn,setGameOn, questions, setQuestions}) {
             <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 3</button></div>
             <div className="answer-btn"><button type="button" onClick={checkAnswer} className="btn btn-primary">Answer 4</button></div>
         </div> 
-    </div> }
+    </div> 
+    }
+    <div className="quit-game">
+            <button type="button" onClick={quitGame} className="btn btn-primary">Quit Game</button>
+            </div>
     </>
 )
 }
